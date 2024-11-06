34f47314af5a75fac8807c8234194bf66c772a37
olivia-diamond <Olivia.diamond@post.runi.ac.il>
2024-11-06 16:07:54 +0200

    1st version Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,22 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
-	}
-}
+	    String name2 = args[1];	
+	String name3 = args[2];
+	int amount = Integer.parseInt(args[3]);
+
+	double split = (double)amount/3; 
+
+	double splitRoundUp = Math.ceil(split);
+
+		//System.out.println("Pay " + splitRoundUp + " Shekels each.");
+
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 +": pay " + splitRoundUp + " Shekels each." );
+
+			}
+				}
+
+	
+	 
+		
+	
