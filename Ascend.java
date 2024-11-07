0c003c7840af32f9af6225d350bdf4a492b0afe0
Olivia Diamond <oliviadiamond@Olivias-MacBook-Air-2.local>
2024-11-07 21:32:29 +0200

    2nd edit ascend

--- Ascend.java
+++ Ascend.java
@@ -4,7 +4,7 @@ public class Ascend {
 	public static void main(String[] args) {
 		// generate 3 random inputs [0;lim)
 
-		int lim = Integer.MAX_VALUE;
+		int lim = Integer.parseInt(args[0]);
 		int a = (int)(Math.random()*lim);
 		int b = (int)(Math.random()*lim);
 		int c = (int)(Math.random()*lim);
